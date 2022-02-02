735b96f2c9558ba8dfc69773048df00c5bd3c35e
Uygar Yilmaz <uygary@anadolu.edu.tr>
2022-02-02 22:07:10 +0100

    nada

--- screenshot.js
+++ screenshot.js
@@ -1,3 +1,3 @@
 import captureWebsite from 'capture-website';
 
-await captureWebsite.file('https://app.purple.is/?e=painttube', `snaps/screenshot_${Date.now()}.png`);
+await captureWebsite.file('https://app.purple.is/?e=painttube', `screenshot_${Date.now()}.png`);
